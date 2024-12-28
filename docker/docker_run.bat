d52e5e0907f1bd9c960487a42d9b1bf0604d6fcb
Jose Francisco López Ruiz <josloprui6@alum.us.es>
2024-12-28 11:03:02 +0100

    Cambiar docker_run.bat

--- docker/docker_run.bat
+++ docker/docker_run.bat
@@ -3,6 +3,6 @@
 set CONTAINER_NAME=airsim_container
 
 docker run --rm -it --net=host ^
--v %CPR_PATH%:/home/testuser/cpr_shared ^
+-v %CPR_PATH%:/home/testuser/ProyectoCPR ^
 --name %CONTAINER_NAME% ^
 airsim-ros:latest bash
